6f932bd6adf5f96a0d2089c48b4e25b65137d46c
Emeka Ugwuanyi <emylincon@gmail.com>
2019-07-31 11:09:04 +0100

    multiple connections done, requires testing

--- mutiple_connection/server.py
+++ mutiple_connection/server.py
@@ -2,6 +2,7 @@
 
 import socket
 import os
+from threading import Thread
 
 
 def ip_address():
@@ -10,33 +11,40 @@ def ip_address():
     return s.getsockname()[0]
 
 
-def unicast_call():
+def receive_data(_con, _addr):
+    with _con:
+        print('Connected: ', _addr)
+        while True:
+            data = _con.recv(1024)
+            print(_addr[0], ': ', data.decode())
+            if data.decode().lower() == 'exit':
+                print('Programme Terminated by Client')
+                break
+
+
+def receive_connection():
     host = ip_address()
     port = 65000        # Port to listen on (non-privileged ports are > 1023)
 
     print('Server IP: {}'.format(host))
 
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((host, port))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
-                print('Connected: ', addr)
-                while True:
-                    data = conn.recv(1024)
-                    print(addr[0], ': ', data.decode())
-                    if data.decode().lower() == 'exit':
-                        print('Programme Terminated by Client')
-                        break
-    except KeyboardInterrupt:
-        print('\nProgramme Forcefully Terminated')
+    while True:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.bind((host, port))
+                s.listen()
+                conn, addr = s.accept()
+
+                Thread(target=receive_data, args=(conn, addr)).start()
+
+        except KeyboardInterrupt:
+            print('\nProgramme Forcefully Terminated')
 
 
 def main():
     os.system('clear')
     print('================== Programme Active =====================\n')
-    unicast_call()
+    receive_connection()
 
 
 if __name__ == "__main__":
